1b545ff7849d20cd98855ebefd96cd33591364d8
Bjoern Kriews <bkr@jumper.org>
2011-08-31 17:16:34 +0200

    bumped version number to 1.1.16r0

--- EngineRoom/er_macros.h
+++ EngineRoom/er_macros.h
@@ -23,8 +23,8 @@
 #ifndef __ER_MACROS_H__
 #define __ER_MACROS_H__
 
-#define _ER_VERSION      1.1.15r1
-#define _ER_VERSION_HEX 0x0111501 
+#define _ER_VERSION      1.1.16r0
+#define _ER_VERSION_HEX 0x0111600 
 /* Apples CFBundleVersion has arcane restrictions 
  Bundle: MMmf.fr.r Hex: 0xMMmffrr MAJORMAJOR.minor.fixfixreleaserelease 
 */
